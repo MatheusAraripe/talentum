5dbcdab7e78e280fe8393624b5beabfe209e0e4e
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-29 11:49:21 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+puts "creating categories"
+Category.destroy_all
+
+Category.create(name: "Photography")
+Category.create(name: "Painting")
+Category.create(name: "Illustration")
+Category.create(name: "Drawing")
+
+
+puts "creating fake users"
+
+User.destroy_all
+
+User.create(first_name: "Pablo", last_name: "Picasso",
+  email: "pablopicasso@email.com", nickname: "pablito", password: "123456", category_id: 2)
+
+User.create(first_name: "Henri", last_name: "Cartier",
+  email: "henricartier@email.com", nickname: "hcb", password: "123456", category_id: 1)
+
+puts "Created all users and categories"
