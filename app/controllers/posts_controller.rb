3bfe4b107f869d995cefeeff1d8724e2aed1e619
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-30 14:54:06 -0300

    feat: add the post controller Co-authored-by: Lorene <lorenemerle@gmail.com> Co-authored-by: Matheus <matheus.alc2001@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  #before_action :set_user, only: %i[new create]
+
+  # before_action :set_user, only: %i[new create]
 
   def new
     @post = Post.new
