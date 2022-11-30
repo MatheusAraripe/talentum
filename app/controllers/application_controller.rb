a15e7e28f2a439dc4af8936e621fea4986b9f78b
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-30 11:40:48 -0300

    feat: add the profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :category_id, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :category_id, :description, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :category_id, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :category_id, :description, :photo])
   end
 end
