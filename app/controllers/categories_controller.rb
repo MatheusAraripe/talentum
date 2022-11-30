554c95d4040cdcce089784f26b3495a54751b0f1
alexandre falcao <alexmfal@gmail.com>
2022-11-30 18:19:46 -0300

    feat: add post to category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @users = User.where(category: @category)
-    # @posts = Post.where(category: @category)
+    @posts = Post.where(category: @category)
   end
 
 end
