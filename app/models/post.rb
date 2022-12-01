e9f8bfc6c88c3c57aaab3501824527c90f311dfa
Lorene Merle <lorenemerle@gmail.com>
2022-12-01 15:02:09 -0300

    feat: creating search method, fixing the seach bar front-end, fixing the home page front-end

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ApplicationRecord
   has_one_attached :photo
 
   validates :title, :category_id, :photo, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description_and_user,
+    against: [ :title, :description ],
+    associated_against: {
+      user: [:nickname]
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
