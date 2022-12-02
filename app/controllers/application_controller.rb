bb8549209a0c3c506203bd644e940bf662ed0ea3
Matheus Araripe <matheus.alc2001@gmail.com>
2022-12-02 11:20:58 -0300

    fix: add links to the nsavbar and change the style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_variables
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :category_id, :description, :photo])
+  end
+
+  def set_variables
+    @photo = Category.find_by(name: "Photography")
+    @painting = Category.find_by(name: "Painting")
+    @illustration = Category.find_by(name: "Illustration")
+    @drawing = Category.find_by(name: "Drawing")
   end
 end
