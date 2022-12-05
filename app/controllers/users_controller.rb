068b935f12ae774823fa0708f297aec91d6e2904
Matheus Araripe <matheus.alc2001@gmail.com>
2022-12-05 11:32:17 -0300

    feat: add users index and follow logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[follow unfollow show]
+
+  def index
+    @users = policy_scope(User)
+    @users = User.where.not(id: current_user.id)
+  end
+
   def show
-    @user = User.find(params[:id])
     @posts = Post.where(user_id: @user)
     authorize @user
+  end
+
+  def follow
+    if current_user.follow(@user.id)
+      authorize @user
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
+  end
+
+  def unfollow
+    if current_user.unfollow(@user.id)
+      authorize @user
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { render action: :follow }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index] do
+    member do
+      post :follow
+      post :unfollow
+    end
+  end
   resources :users, only: [:show]
   resources :categories, only: [:show]
   resources :posts, only: [:new, :create, :show] do
