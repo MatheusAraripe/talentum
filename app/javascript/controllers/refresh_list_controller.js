99f64e8790e5f82147cc9adc6c2ef9beea8a1bb1
Matheus de Araripe Lopes Corrêa <90289904+MatheusAraripe@users.noreply.github.com>
2022-12-06 11:29:22 -0300

    Update refresh_list_controller.js

--- app/javascript/controllers/refresh_list_controller.js
+++ app/javascript/controllers/refresh_list_controller.js
@@ -3,14 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "form", "list", "searchInput" ]
 
-  connect() {
-    console.log(this.element);
-    console.log(this.formTarget);
-    console.log(this.listTarget);
-    console.log(this.searchInputTarget);
-    console.log('working')
-  }
-
   update() {
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
